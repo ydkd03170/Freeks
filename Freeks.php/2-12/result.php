e9dfb167abad24511e4272e14ebf3743e590917c
ydkd03170 <85867612+ydkd03170@users.noreply.github.com>
2021-12-14 21:35:13 +0900

    Delete Freeks.php directory

--- Freeks.php/2-12/result.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $my_name = $_POST['my_name'];
-    $gift = $_POST['gift'];
-    $number = $_POST['number'];
-    ?>
-    <p>お名前：<?php echo $my_name ; ?></p>
-    <p>ご希望景品：<?php echo $gift ; ?></p>
-    <p>個数：<?php echo $number ; ?></p>
--- Freeks.php/2-7/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$color = ["red" => "赤", "blue" => "青", "green" => "緑
-"];
-var_dump($color);
-echo('<br>');
-$color["yellow"] = "黄色";
-var_dump($color);
-?>
