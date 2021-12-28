b49ddbd5a08f902f3b746907a998fc01f06981a7
ydkd03170 <85867612+ydkd03170@users.noreply.github.com>
2021-12-28 15:58:26 +0900

    Add files via upload

--- 2-17/index.php
+++ 2-17/index.php
@@ -1,22 +1,21 @@
 <?php
-define("GOAL", 40);
-$total = 0;
-for ($i = 1; $i <= GOAL; $i++) {
-    $n = rand(1, 6);
-    $total += $n;
-    echo "{$i}回目={$n}\n";
-    if ($total >= GOAL) {
-        echo "合計{$i}回でゴールしました\n";
-        break;
-    }
+$x = 0;
+$i = 0;
+while ($x <= 40){
+  $i++;
+  $s = rand(1,6);
+  echo $i ."回目=". $s."\n";
+  $x += $s;
 }
+echo "合計".$i."回でゴールしました";
 ?>
 <br>
 <?php
+date_default_timezone_set('Asia/Tokyo');
 $time = intval(date('H'));
-if (4 <= $time && $time <= 9) { // 4時～12時の時間帯のとき ?>
+if (4 <= $time && $time <= 12) { // 4時～12時の時間帯のとき?>
 <p>おはようございます。</p>
-<?php } elseif (10 <= $time && $time <= 15) { // 12時〜20時の時間帯のとき ?>
+<?php } elseif (12 <= $time && $time <= 20){ // 12時〜20時の時間帯のとき ?>
 <p>こんにちわ。</p>
 <?php } else { // それ以外の時間帯のとき ?>
 <p>こんばんわ。</p>
